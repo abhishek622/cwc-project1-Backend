663f2c2288d41713d34519881426e33b60572d13
Abhishek Prasad <53276705+abhishek622@users.noreply.github.com>
2021-08-23 10:53:47 +0530

    Update paymentRoute.js

--- paymentRoute.js
+++ paymentRoute.js
@@ -29,7 +29,6 @@ router.post('/orders', async (req, res) => {
       res.json(order);
     });
 
-    // if (!order) return res.status(500).send("Some error occured");
   } catch (error) {
     res.status(500).send(error);
   }
@@ -37,7 +36,7 @@ router.post('/orders', async (req, res) => {
 
 router.post('/success', async (req, res) => {
   try {
-    // getting the details back from our font-end
+   
     const {
       orderCreationId,
       razorpayPaymentId,
@@ -45,21 +44,14 @@ router.post('/success', async (req, res) => {
       razorpaySignature,
     } = req.body;
 
-    // Creating our own digest
-    // The format should be like this:
-    // digest = hmac_sha256(orderCreationId + "|" + razorpayPaymentId, secret);
-
     const hash = crypto
       .createHmac('sha256', process.env.SECRET_KEY)
       .update(`${orderCreationId}|${razorpayPaymentId}`)
       .digest('hex');
 
-    // comaparing our digest with the actual signature
     if (hash !== razorpaySignature)
       return res.status(400).json({ msg: 'Transaction not legit!' });
 
-    // THE PAYMENT IS LEGIT & VERIFIED
-    // YOU CAN SAVE THE DETAILS IN YOUR DATABASE IF YOU WANT
     res.json({
       msg: 'success',
       orderId: razorpayOrderId,
